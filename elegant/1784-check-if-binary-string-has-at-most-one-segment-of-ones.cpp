d20265505a9c853390812ddde48adbb1331a2145
LS_Hower <ls_hower@qq.com>
2026-03-06 10:42:01 +0800

    Update 1784

--- elegant/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ elegant/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
-    bool checkOnesSegment(string s) {
-        return ranges::all_of(
-            ranges::find(s, '1'),
-            ranges::find_last(s, '1').begin(),
-            [](char c) { return c == '1'; }
-        );
+    bool checkOnesSegment(string_view s) {
+        return ranges::is_sorted(s, greater{});
     }
 };
