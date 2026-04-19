437b1a7a4694d91e49285a561d672ebc56352934
LS_Hower <ls_hower@qq.com>
2026-04-20 00:37:55 +0800

    Update 2078

--- elegant/2078-two-furthest-houses-with-different-colors.rkt
+++ elegant/2078-two-furthest-houses-with-different-colors.rkt
@@ -1,9 +1,9 @@
 (define/contract (max-distance colors)
   (-> (listof exact-integer?) exact-integer?)
-  (let ([rev-colors (reverse colors)])
-    (let* ([n (length colors)]
-           [left (first colors)]
-           [right (first rev-colors)]
-           [l (index-where colors (lambda (c) (not (= c right))))]
-           [r (index-where rev-colors (lambda (c) (not (= c left))))])
-      (- n 1 (min l r)))))
+  (let* ([n (length colors)]
+         [rev-colors (reverse colors)]
+         [left (first colors)]
+         [right (first rev-colors)]
+         [l (index-where colors (lambda (c) (not (= c right))))]
+         [r (index-where rev-colors (lambda (c) (not (= c left))))])
+    (- n 1 (min l r))))
