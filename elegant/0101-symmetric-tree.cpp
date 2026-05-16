f3e540204c2f0775b05977a66624f947e34d48db
LS_Hower <ls_hower@qq.com>
2026-05-17 00:36:05 +0800

    Add 101

---
+++ elegant/0101-symmetric-tree.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root)
+    {
+        return [](this auto me, TreeNode* l, TreeNode* r) -> bool {
+            return !(l || r) || l && r && l->val == r->val && me(l->left, r->right) && me(l->right, r->left);
+        }(root->left, root->right);
+    }
+};
